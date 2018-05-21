75c9affdbb9f5effce6b4f4a99e02be53b9be82e
João Santos <joaoecoceanus@gmail.com>
2018-05-21 19:36:04 +0100

    Add navbar partial

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,39 @@
+<nav class="flex justify-end">
+
+    <ul class="flex list-reset mt-4 bg-red">
+
+        <li class="mx-4">
+            <a href="">Diving</a>
+            {{-- Diving Dropdown --}}
+            <ul class="list-reset bg-blue">
+                <li>
+                    <a href="">Courses</a>
+                </li>
+                <li>
+                    <a href="">Trips</a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="mx-4">
+            <a href="">Snorkel</a>
+        </li>
+
+        <li class="mx-4">
+            <a href="">Research</a>
+        </li>
+
+        <li class="mx-4">
+            <a href="">About</a>
+        </li>
+
+        <li class="mx-4">
+            <a href="">Gallery</a>
+        </li>
+
+        <li class="mx-4">
+            <a href="">Contacts</a>
+        </li>
+
+    </ul>
+</nav>
