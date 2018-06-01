389c53cfea54f716ca227c8d22fa6c0c8d118361
João Santos <joaoecoceanus@gmail.com>
2018-06-01 21:45:20 +0100

    Created a blade component for card profile

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -27,70 +27,10 @@
     <div class="flex mb-6 border-b-2 border-grey-light">
         <h1 class="self-start font-semibold text-2xl border-b-2 border-blue-light -mb-2px">Staff</h1>
     </div>
-    {{-- Profile card --}}
-    <div class="staff-card flex flex-col bg-white">
 
-        {{-- Image, name and working position--}}
-        <img src="/img/staff/joao_rodrigues_opt.jpg" alt="João Rodrigues">
+    @component('components.profile-card')
 
-        <div class="p-8 flex flex-col items-center">
-            <h2 class="font-semibold mb-2">João Rodrigues</h2>
-
-            <h3 class="font-normal text-sm text-grey-darker antialiased mb-4">Co-founder | Technical director</h3>
-
-            {{-- social links --}}
-            <div class="flex justify-center mb-4 pb-4 border-b w-4/5 text-grey-light">
-                <a href="#">
-                    <i class="fab fa-instagram mx-2 fa-2x hover:text-grey-darker"></i>
-                </a>
-                <a href="#">
-                    <i class="fab fa-facebook-square mx-2 fa-2x hover:text-grey-darker"></i>
-                </a>
-                <a href="#">
-                    <i class="fas fa-link mx-2 fa-2x hover:text-grey-darker"></i>
-                </a>
-            </div>
-
-            {{-- Acomplishments --}}
-            <div class="flex flex-col mb-2 border-b  w-4/5">
-                <h4 class="mb-2 self-start text-blue-light font-semibold">Diving certifications</h4>
-                <ul class="list-reset">
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">PADI Open Water Scuba Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">PADI Enriched Air Nitrox Specialty Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">PADI Tec Gas Blender Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">PADI EFR Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">ITD Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">RAID Rebreather diver level 4</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">GUE Technical diver level 1</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">GUE Cave diver level 1</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">CMAS Scientific diver</li>
-                </ul>
-            </div>
-
-            <div class="flex flex-col mb-2 border-b w-4/5">
-                <h4 class="mb-2 self-start text-blue-light font-semibold">Nautical certifications</h4>
-                <ul class="list-reset">
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">Local Skipper</li>
-                </ul>
-            </div>
-
-            <div class="flex flex-col mb-2 border-b w-4/5">
-                <h4 class="mb-2 self-start text-blue-light font-semibold">Academic formation</h4>
-                <ul class="list-reset">
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">Degree in Biology</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">Master in Marine Biodiversity and Conservation</li>
-                </ul>
-            </div>
-
-            <div class="flex flex-col mb-2 border-b w-4/5">
-                <h4 class="mb-2 self-start text-blue-light font-semibold">Other</h4>
-                <ul class="list-reset">
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">Natural History and Conservation Photojournalist</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    {{-- End of Profile card --}}
+    @endcomponent
 
 </section>
 
