d6bcfb75113338cdd9aa1c133872c92ebae56757
João Santos <joaoecoceanus@gmail.com>
2018-06-01 23:12:21 +0100

    Created Card Component directive, added slots to the card.

--- resources/views/components/profile-card.blade.php
+++ resources/views/components/profile-card.blade.php
@@ -1,13 +1,19 @@
     {{-- Profile card --}}
-    <div class="staff-card flex flex-col bg-white">
+    <div class="staff-card flex flex-col bg-white m-4">
 
-        {{-- Image, name and working position--}}
-        <img src="/img/staff/joao_rodrigues_opt.jpg" alt="João Rodrigues">
+        {{-- Profile pic --}}
+        <img src="/img/staff/{{ $imgUrl }}" alt="{{ $name }}">
 
+        {{-- Content --}}
         <div class="p-8 flex flex-col items-center">
-            <h2 class="font-semibold mb-2">João Rodrigues</h2>
 
-            <h3 class="font-normal text-sm text-grey-darker antialiased mb-4">Co-founder | Technical director</h3>
+            <h2 class="font-semibold mb-2">
+                {{ $name }}
+            </h2>
+
+            <h3 class="font-normal text-sm text-grey-darker antialiased mb-4">
+                {{ $position }}
+            </h3>
 
             {{-- social links --}}
             <div class="flex justify-center mb-4 pb-4 border-b w-4/5 text-grey-light">
@@ -22,43 +28,52 @@
                 </a>
             </div>
 
-            {{-- Acomplishments --}}
+
             <div class="flex flex-col mb-2 border-b  w-4/5">
-                <h4 class="mb-2 self-start text-blue-light font-semibold">Diving certifications</h4>
+
+                <h4 class="mb-2 self-start text-blue-light font-semibold">
+                    Diving certifications
+                </h4>
+
                 <ul class="list-reset">
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">PADI Open Water Scuba Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">PADI Enriched Air Nitrox Specialty Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">PADI Tec Gas Blender Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">PADI EFR Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">ITD Instructor</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">RAID Rebreather diver level 4</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">GUE Technical diver level 1</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">GUE Cave diver level 1</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">CMAS Scientific diver</li>
+                    {{ $divingCertifications }}
                 </ul>
+
             </div>
 
             <div class="flex flex-col mb-2 border-b w-4/5">
-                <h4 class="mb-2 self-start text-blue-light font-semibold">Nautical certifications</h4>
+
+                <h4 class="mb-2 self-start text-blue-light font-semibold">
+                    Nautical certifications
+                </h4>
                 <ul class="list-reset">
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">Local Skipper</li>
+                    {{ $nauticalCertifications }}
                 </ul>
+
             </div>
 
             <div class="flex flex-col mb-2 border-b w-4/5">
-                <h4 class="mb-2 self-start text-blue-light font-semibold">Academic formation</h4>
+
+                <h4 class="mb-2 self-start text-blue-light font-semibold">
+                    Academic formation
+                </h4>
                 <ul class="list-reset">
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">Degree in Biology</li>
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">Master in Marine Biodiversity and Conservation</li>
+                    {{ $academics }}
                 </ul>
+
             </div>
 
             <div class="flex flex-col mb-2 border-b w-4/5">
-                <h4 class="mb-2 self-start text-blue-light font-semibold">Other</h4>
+
+                <h4 class="mb-2 self-start text-blue-light font-semibold">
+                    Other
+                </h4>
                 <ul class="list-reset">
-                    <li class="antialiased text-xs font-semibold text-grey-darker mb-3">Natural History and Conservation Photojournalist</li>
+                    {{ $other }}
                 </ul>
+
             </div>
+
         </div>
-    </div>
-    {{-- End of Profile card --}}
+
+    </div> {{-- End of Profile card --}}
