688448b05415f28db1b22fa5c11bd20b88d11703
João Santos <joaoecoceanus@gmail.com>
2018-07-01 18:10:27 +0100

    Fixed homepage responsive layout

--- resources/views/layout/landing.blade.php
+++ resources/views/layout/landing.blade.php
@@ -1,6 +1,6 @@
 @include('partials.landing-header')
 
-<div id="wrapper" class="mx-auto px-8 md:px-6 lg:px-0 opacity-0 w-full flex-1">
+<div id="wrapper" class="mx-auto px-8 md:px-6 lg:px-4 w-full flex-1 opacity-1">
     @yield('content')
 </div>
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-<div>
-    <h1 class="text-white font-light uppercase tracking-wide text-5xl leading-normal mt-10">
+<div class="md:w-4/5 lg:w-full mx-auto">
+    <h1 class="text-white font-semibold uppercase font-sans tracking-wide text-xl leading-normal mt-10 leading-loose sm:text-2xl md:text-3xl lg:text-4xl lg:pt-10">
         <span class="italic">"</span>
             The sea, once it casts its spell, holds one in its net of wonder forever.
         <span class="italic">"</span>
     </h1>
 
-    <h2 class="text-grey-light font-normal tracking-wide text-base italic antialiased">Jacques Yves Cousteau</h2>
+    <h2 class="text-blue text-xs font-semibold tracking-wide italic antialiased">Jacques Yves Cousteau</h2>
 </div>
 
 @endsection
